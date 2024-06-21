b1287f38b6f9140d04c2cab05ba4fadc4b71ea4c
Sanzhar Akparaliev <taalaibekuulusanzhar@gmail.com>
2024-06-21 18:56:45 +0600

    translation

--- src/main/java/com/axelor/script/module/AxelorScript.java
+++ src/main/java/com/axelor/script/module/AxelorScript.java
@@ -1,8 +1,10 @@
 package com.axelor.script.module;
 
 import com.axelor.app.AxelorModule;
+import com.axelor.script.service.DictionaryService;
 import com.axelor.script.service.FieldService;
 import com.axelor.script.service.PermissionService;
+import com.axelor.script.service.impl.DictionaryServiceImpl;
 import com.axelor.script.service.impl.FieldServiceImpl;
 import com.axelor.script.service.impl.PermissionServiceImpl;
 
@@ -11,5 +13,6 @@ public class AxelorScript extends AxelorModule {
     protected void configure() {
         bind(PermissionService.class).to(PermissionServiceImpl.class);
         bind(FieldService.class).to(FieldServiceImpl.class);
+        bind(DictionaryService.class).to(DictionaryServiceImpl.class);
     }
 }
---
+++ src/main/java/com/axelor/script/service/DictionaryService.java
@@ -0,0 +1,5 @@
+package com.axelor.script.service;
+
+public interface DictionaryService {
+    String getTranslation(String from, String to, String request);
+}
