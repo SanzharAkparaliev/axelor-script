f5912dde1833d8f93ba7f57cc0d3c80f13b0eef3
Sanzhar Akparaliev <taalaibekuulusanzhar@gmail.com>
2024-07-30 12:21:49 +0600

    change view

---
+++ build/src-gen/java/com/axelor/message/db/repo/TemplateRepository.java
@@ -0,0 +1,29 @@
+package com.axelor.message.db.repo;
+
+import com.axelor.db.JpaRepository;
+import com.axelor.db.Query;
+import com.axelor.message.db.Template;
+
+public class TemplateRepository extends JpaRepository<Template> {
+
+  public TemplateRepository() {
+    super(Template.class);
+  }
+
+  public Template findByName(String name) {
+    return Query.of(Template.class)
+      .filter("self.name = :name")
+      .bind("name", name)
+      .fetchOne();
+  }
+
+  // MEDIA TYPE SELECT
+  public static final int MEDIA_TYPE_MAIL = 1;
+  public static final int MEDIA_TYPE_EMAIL = 2;
+  public static final int MEDIA_TYPE_CHAT = 3;
+  public static final int MEDIA_TYPE_SMS = 5;
+
+  // TEMPLATE ENGINE TYPE
+  public static final int TEMPLATE_ENGINE_STRING_TEMPLATE = 1;
+  public static final int TEMPLATE_ENGINE_GROOVY_TEMPLATE = 2;
+}
